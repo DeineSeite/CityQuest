fcaea1ad98a6f008482767198fe2f0de789dfe6c
Vladyslav Kostiushko <vladik1996@live.ru>
2016-12-08 23:50:00 +0100

    CQ-6 Navigations between pages

---
+++ CityQuest/CityQuest/Pages/MenuPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CityQuest.Pages
+{
+    public partial class MenuPage : BasePage
+    {
+        public View MenuContentView
+        {
+            get { return MenuView.Content; }
+            set { MenuView.Content = value; }
+        }
+
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
