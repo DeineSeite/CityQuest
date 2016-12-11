bbc651154884981bb9cd634907a37307d69afe82
Vladyslav Kostiushko <vladik1996@live.ru>
2016-12-11 10:33:46 +0100

    Library fix

--- CityQuest/CityQuest/Pages/StartPage.xaml.cs
+++ CityQuest/CityQuest/Pages/StartPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Compass;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +16,16 @@ namespace CityQuest.Pages
         public StartPage()
         {
             InitializeComponent();
-            QuestWebView.Source = "http://app.deineseite.at/cityquest/TestQuest/Quest1.html";
+            QuestWebView.Source = "http://app.deineseite.at/cityquest/TestQuest/Compass.html";
+            CrossCompass.Current.CompassChanged += (s, e) =>
+            {
+                Debug.WriteLine("*** Compass Heading = {0}", Math.Round(e.Heading, 5));
+
+                QuestWebView.Eval(string.Format("rotateCompass({0})", Math.Round(e.Heading,5)));
+
+            };
+
+            CrossCompass.Current.Start();
         }
     }
 }
